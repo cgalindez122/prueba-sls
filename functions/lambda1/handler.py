be7e4f1b42b2e769da102f83e04eff90156837b6
Carlos Galindez <cgalindez12@gmail.com>
2024-06-25 09:15:40 -0500

    change

--- functions/lambda1/handler.py
+++ functions/lambda1/handler.py
@@ -2,5 +2,5 @@ def handler(event, context):
     print("Hello from Lambda1!")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda1!')
+        'body': json.dumps('Hello from Lambda1!-change')
     }
